4a07e8d8d364326c9755df55aa50dacfbb2e2a48
tusarmolla96@outlook.com <tusarmolla96@gmail.com>
2025-04-08 23:10:12 +0600

    Company Controller Done

---
+++ StockSense/Controllers/CompanyController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StockSense.Interface;
+using StockSense.Models;
+
+namespace StockSense.Controllers
+{
+    public class CompanyController : Controller
+    {
+        private readonly ILogger<CompanyController> _logger;
+        private readonly ICompanyService _companyService;
+
+        public CompanyController(ILogger<CompanyController> logger, ICompanyService companyService)
+        {
+            _logger = logger;
+            _companyService = companyService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Company()
+        {
+            var company = await _companyService.GetAllAsync();
+            if (company == null)
+            {
+                return View(new Company());
+            }
+            return View(company);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Company(Company company)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(company);
+            }
+            try
+            {
+                if (company.Id == 0)
+                {
+                    bool isCreated = await _companyService.CreateCompanyAsync(company);
+                    if (!isCreated)
+                    {
+                        TempData["ErrorMessage"] = "An Error Occour when Creating Company";
+                        return View(company);
+                    }
+                }
+                else
+                {
+                    bool isUpdated = await _companyService.UpdateCompanyAsync(company);
+                    if (!isUpdated)
+                    {
+                        TempData["ErrorMessage"] = "Company name already exists or update failed";
+                        return View(company);
+                    }
+                }
+                TempData["SuccessMessage"] = "Company Created Successfully";
+                return RedirectToAction("Company");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, "An error occurred: " + ex.Message);
+                return View(company);
+            }
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
