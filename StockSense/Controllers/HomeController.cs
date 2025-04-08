ff7ce27d502120cef877f7a5d4cfe3cbec351577
tusarmolla96@outlook.com <tusarmolla96@gmail.com>
2025-04-08 22:23:07 +0600

    Dashboard view and Authentication process done

--- StockSense/Controllers/HomeController.cs
+++ StockSense/Controllers/HomeController.cs
@@ -1,9 +1,10 @@
 using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using StockSense.Models;
 
 namespace StockSense.Controllers;
-
+[Authorize]
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
--- StockSense/Program.cs
+++ StockSense/Program.cs
@@ -8,10 +8,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
 // Register services
-
 builder.Services.AddScoped<IAuthService, AuthService>();
-
+builder.Services.AddScoped<BaseService>();
+builder.Services.AddHttpContextAccessor();
 // Configure DbContext with resilience
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("AccountConnection")));
 // Configure cookie authentication
@@ -33,18 +34,18 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+else
+{
+    app.UseDeveloperExceptionPage();
+}
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
-
-app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
-
+    pattern: "{controller=Home}/{action=Dashboard}/{id?}");
 app.Run();
